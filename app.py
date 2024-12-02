a4d2e5cbbffac62bf431a4ccdb87007bfff04324
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2024-12-02 19:56:19 -0400

    done

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home route renders index.html
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# UXDesign route renders UXDesign.html
+
+
+@app.route('/UXDesign')
+def ux_design():
+    return render_template('UXDesign.html')
+
+# Contact route renders contact.html
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
